716c5dc985288f4d8c037e6273cd2f4ad19a070d
Ignacio MIllapani <misttiski@gmail.com>
2025-06-03 13:03:52 -0400

    Se realizo el lab 2, comienzo con operadores aritmeticos

---
+++ GUIAS DE APRENDIZAJE/07-Operadores-aritmericos.py
@@ -0,0 +1,23 @@
+#PRUEBA DE VALORES INMUTABLES 
+a = 100
+print(f"El valor de a es {a}")
+
+b = a 
+print(f"El valor de b es {b}") 
+
+c = b 
+print(f"El valor de c es {c}") #El valor se mantiene
+
+#EXPRESIONES DE VARIABLES DE TIPO ENTERO 
+a = 10 
+b = 20 
+c = 30 
+d = 40 
+
+#OPERACIONES COMUNES 
+print("#### 01. OPERADORES ARITMETICOS ####")
+print(f"Suma entre a y b es: {a + b}")
+
+#OPERACIONES DE VARIABLES DE TIPO COMPLEJO
+com = 4+ 1j
+print(type(com))
