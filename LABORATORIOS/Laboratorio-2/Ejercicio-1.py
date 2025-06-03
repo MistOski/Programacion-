4eb32ed3ccc13bd25cc1b3490f20d4964feea6a4
Ignacio MIllapani <misttiski@gmail.com>
2025-06-03 10:53:15 -0400

    lab

---
+++ LABORATORIOS/Laboratorio-2/Ejercicio-1.py
@@ -0,0 +1,63 @@
+#Una ferretería necesita desarrollar un programa que permita gestionar el stock de productos de 
+#su tienda. La ferretería requiere saber el total a pagar por la cantidad de herramientas 
+#ingresadas a su bodega. (50 Puntos)
+
+#Pedir por consola los precios unitarios (float) y las cantidades (int) de cuatro productos:
+
+#Martillo 
+pma = float(input("Ingrese el precio unitario del martillo: "))
+cma = int(input("Ingrese la cantidad disponible de martillos: "))
+
+#Clavos
+pcl = float(input("Ingrese el precio unitario de los  clavos: "))
+ccl = int(input("Ingrese la cantidad disponible de los clavos: "))
+
+#Serrucho 
+pse = float(input("Ingrese el precio unitario del serrucho: "))
+cse = int(input("Ingrese la cantidad disponible del serruchos: "))
+
+#Tornillos
+pto = float(input("Ingrese el precio unitario de los tornillos: "))
+cto = int(input("Ingrese la cantidad disponible de los martillos: "))
+
+#Calcular y mostrar los subtotales para cada producto y redondearlos a 2 decimales.
+sma = pma * cma #martillo
+
+scl = pcl * ccl #clavos
+
+sse = pse * cse #serrucho
+
+sto = pto * cto #
+
+
+#Imprimir los siguientes resultados, en este orden: 
+
+#Subtotal Martillo | Subtotal Clavos | Subtotal Serrucho | Subtotal Tornillos 
+# b. Suma total de los cuatro subtotales. 
+# c. 
+# Valor máximo entre los cuatro subtotales. 
+# d. Valor mínimo entre los cuatro subtotales. 
+# e. Promedio del precio unitario de las herramientas, redondeado a 2 decimales 
+# f. 
+# Obtener el IVA (19%) del total de las herramientas ingresadas al sistema. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
